8ecb0a76ad3f2867034feb4302a9fe5d934a974a
nasenloch63 <yasin.aissani@gmx.de>
2026-08-05 11:37:50 +0200

    haeufigkeit

---
+++ haeufigkeit.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main (void) 
+{
+    int zahlen[] = {2, 5, 2, 8, 5, 2, 0, 8, 9};
+
+    int laenge = sizeof/zahlen) / sizeof(zahlen[0]);
+
+    int hauefigkeit[10] } {0};
+
+    for( )
+}
