54a69cef0be7813b3048c0ed588eb0ba44f6e6e1
nasenloch63 <yasin.aissani@gmx.de>
2026-08-05 07:35:03 +0200

    array reverse

---
+++ array_reverse.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(void) {
+    int array[] = {4, 8, 15, 12};
+    
+    int laenge = sizeof(array) / sizeof(array[0]);
+    int umgekehrt[laenge];
+
+    // Erste SChleife: Werte rückwärts kopieren
+
+    // Zweite Schleife: neues Array ausgeben
+return 0;
+}
