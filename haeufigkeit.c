825a2012d14b231101892349d5578351e9c43eac
nasenloch63 <yasin.aissani@gmx.de>
2026-08-05 13:33:33 +0200

    heaufigkeit mit ausgabe

--- haeufigkeit.c
+++ haeufigkeit.c
@@ -3,9 +3,20 @@ int main (void)
 {
     int zahlen[] = {2, 5, 2, 8, 5, 2, 0, 8, 9};
 
-    int laenge = sizeof/zahlen) / sizeof(zahlen[0]);
+    int laenge = sizeof(zahlen) / sizeof(zahlen[0]);
 
-    int hauefigkeit[10] } {0};
+    int hauefigkeit[10] = {0};
 
-    for( )
+    for(int i = 0; i < 10; i++)
+    { 
+        hauefigkeit[zahlen[i]]++;  
+    }
+    for (int i = 0; i < 10; i++)
+    { 
+        printf(
+            "Die Zahl %d kommt %d-mal vor. \n",
+            i,
+            hauefigkeit[i]
+        );
+    }
 }
