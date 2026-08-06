9c3d208ba12ddf1a810054459c55543a30f756cc
nasenloch63 <yasin.aissani@gmx.de>
2026-08-06 13:45:39 +0200

    palindrom berechnung

---
+++ palindrom.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(void) {
+    char text[200];
+    printf("Text eingeben: ");
+    fgets(text, sizeof(text), stdin);
+    int laenge = 0;
+    while (text[laenge] != "\0")
+    {
+        laenge++
+    }
+}
