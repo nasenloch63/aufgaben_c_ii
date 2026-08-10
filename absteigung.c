c2abb755b57198fc6297ee7922d64ce234d6230f
nasenloch63 <yasin.aissani@gmx.de>
2026-08-10 15:10:19 +0200

    review

--- absteigung.c
+++ absteigung.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
- int main(void) 
- {
-    for (int i = 20; i >=2; i-= 2)
-    {printf("%d\n", i);
-        /* code */
+
+int main(void) 
+{
+    for (int i = 20; i >= 2; i -= 2) {
+        printf("%d\n", i);
     }
     return 0;
- }
+}
--- haeufigkeit.c
+++ haeufigkeit.c
@@ -1,22 +1,21 @@
 #include <stdio.h>
-int main (void) {
-    int zahlen[] = {2, 5, 2, 8, 5, 2, 0, 8, 9};
+int main(void)
+{
+    int zahlen[] = {2, 5, 2, 8, 5, 2, 4, 5, 4, 4, 4, 3, 2, 1, 2, 1, 1, 5, 2, 2, 3, 1, 2, 3, 9, 0, 1, 3};
 
     int laenge = sizeof(zahlen) / sizeof(zahlen[0]);
 
     int hauefigkeit[10] = {0};
 
-    for(int i = 0; i < 10; i++)
-    { 
-        hauefigkeit[zahlen[i]]++;  
+    for (int i = 0; i < laenge; i++) {
+        hauefigkeit[zahlen[i]]++;
     }
-    for (int i = 0; i < 10; i++)
-    { 
+    
+    for (int i = 0; i < 10; i++) {
         printf(
             "Die Zahl %d kommt %d-mal vor. \n",
             i,
-            hauefigkeit[i]
-        );
+            hauefigkeit[i]);
     }
-return 0;
+    return 0;
 }
